362a14987a38b50d010a8f21345b25f7a48c21d3
Jashmaan <107420467+Jashmaan@users.noreply.github.com>
2025-07-02 18:26:19 +0530

    Update Assignment4_task1.py

--- Assignment4_task1.py
+++ Assignment4_task1.py
@@ -5,3 +5,12 @@ content_read2 = sample.readline().strip()
 print ("Line 1: ", content_read)
 print ("Line 2: ", content_read2)
 sample.close()
+
+
+#error message cleanely that the file samples.txt was not found, in case file does not exist
+try:
+    samples = open ("samples.txt", "r")
+    samples.close()
+except FileNotFoundError:
+     print("Error: The file 'samples.txt' was not found.")
+    
